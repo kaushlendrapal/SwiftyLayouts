ffde2800939ec439a0c058756842c1579b0524fa
kaushal <kaushal@Abhisheks-Mac-mini.local>
2018-01-17 16:38:36 +0530

    added example project

---
+++ SwiftyLayoutsExample/SwiftyLayoutsExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyLayoutsExample
+//
+//  Created by kaushal on 17/01/18.
+//  Copyright © 2018 kaushal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
