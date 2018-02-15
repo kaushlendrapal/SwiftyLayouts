1f934aff3a621b36f7e6d86c7c01d6aa7c4a871a
Kaushlendra pal <kaushalyuvi@gmail.com>
2018-02-15 11:49:45 +0530

    final commit for universal fat framework

--- SwiftyLayouts/Products/Debug-universal/SwiftyLayouts.framework/swiftyLayouts_universal_fat.sh
+++ SwiftyLayouts/Products/Debug-universal/SwiftyLayouts.framework/swiftyLayouts_universal_fat.sh
@@ -6,58 +6,44 @@
 #  Created by kaushal on 13/02/18.
 #  Copyright © 2018 kaushal. All rights reserved.
 
-#!/bin/bash
+UNIVERSAL_OUTPUTFOLDER=${BUILD_DIR}/${CONFIGURATION}-universal
 
-# Adapted from http://stackoverflow.com/questions/24039470/xcode-6-ios-creating-a-cocoa-touch-framework-architectures-issues/26691080#26691080
-# and https://gist.github.com/cromandini/1a9c4aeab27ca84f5d79
-
-# Create a new aggregate target.
-# For the automatically generated scheme, change its build config to "release".
-# Ensure this target's "product name" build setting matches the framework's.
-# Add a run script with `source "${PROJECT_DIR}/path_to_this_script`
-
-UNIVERSAL_OUTPUT_DIR=${BUILD_DIR}/${CONFIGURATION}-universal
 RELEASE_DIR=${PROJECT_DIR}/Products/${CONFIGURATION}-universal
 
-# Remove universal folder and its directory if exist
+# clear the old cache i universal framework
 rm -rf "${UNIVERSAL_OUTPUTFOLDER}"
-rm -rf "${RELEASE_DIR}"
 
 # make sure the output directory exists
-mkdir -p "${UNIVERSAL_OUTPUT_DIR}"
+mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
 mkdir -p "${RELEASE_DIR}"
 
 # Step 1. Build Device and Simulator versions
 echo "=== Building for iPhone device ==="
-xcodebuild -target "${PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+xcodebuild -target "${PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos  BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
 
 echo "=== Building for iPhoneSimulator ==="
 xcodebuild -target "${PROJECT_NAME}" -configuration ${CONFIGURATION} -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO -arch x86_64 -destination 'platform=iOS Simulator,name=iPhone 8' BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
 
+echo "=== coping iPhone framework to universal ==="
 # Step 2. Copy the framework structure (from iphoneos build) to the universal folder
-cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework" "${UNIVERSAL_OUTPUT_DIR}/"
+cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
+# Step 3. Copy the dSYM structure (from iphoneos build) to the universal folder
+cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework.dSYM" "${UNIVERSAL_OUTPUTFOLDER}/"
 
-# Step 5. Copy the dSYM structure (from iphoneos build) to the universal folder
-cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework.dSYM" "${UNIVERSAL_OUTPUT_DIR}/"
+echo "=== coping swift module to universal ==="
+# Step 4. Copy Swift modules (from iphonesimulator build) to the copied framework directory
+cp -R "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PROJECT_NAME}.framework/Modules/${PROJECT_NAME}.swiftmodule/." "${UNIVERSAL_OUTPUTFOLDER}/${PROJECT_NAME}.framework/Modules/${PROJECT_NAME}.swiftmodule"
 
-# Step 3. Copy Swift modules from iphonesimulator build (if it exists) to the copied framework directory
-SIMULATOR_SWIFT_MODULES_DIR="${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PROJECT_NAME}.framework/Modules/${PROJECT_NAME}.swiftmodule/."
-if [ -d "${SIMULATOR_SWIFT_MODULES_DIR}" ]; then
-cp -R "${SIMULATOR_SWIFT_MODULES_DIR}" "${UNIVERSAL_OUTPUT_DIR}/${PROJECT_NAME}.framework/Modules/${PROJECT_NAME}.swiftmodule/"
-fi
-
-# Step 4. Create universal binary file using lipo and place the combined executable in the copied framework directory
+# Step 5. Create universal binary file using lipo and place the combined executable in the copied framework directory
 echo "=== Combining executables ==="
 lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework/${PROJECT_NAME}"
 
+rm -rf "${RELEASE_DIR}/${PROJECT_NAME}.framework"
 
-# Step 7. Remove existing builds
-#rm -rf "${RELEASE_DIR}"
-
-# Step 5. Convenience step to copy the framework to the project's directory
+# Step 6. Convenience step to copy the framework to the project's directory
 echo "===Copying to project dir ==="
-cp -R "${UNIVERSAL_OUTPUT_DIR}/${PROJECT_NAME}.framework" "${RELEASE_DIR}"
-cp -R "${UNIVERSAL_OUTPUT_DIR}/${PROJECT_NAME}.framework.dSYM" "${RELEASE_DIR}"
+cp -R "${UNIVERSAL_OUTPUTFOLDER}/${PROJECT_NAME}.framework" "${RELEASE_DIR}"
+cp -R "${UNIVERSAL_OUTPUTFOLDER}/${PROJECT_NAME}.framework.dSYM" "${RELEASE_DIR}"
 
-# Step 6. Convenience step to open the project's directory in Finder
+# Step 7. Convenience step to open the project's directory in Finder
 open "${RELEASE_DIR}"
